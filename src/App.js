9a422fcf021215447332d22fbd9f753a0791ffbc
EC2 Default User <ec2-user@ip-172-31-80-100.ec2.internal>
2020-09-15 18:28:57 +0000

    version4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Amplify, { Storage } from 'aws-amplify';
+//import awsconfig from './aws-exports';
+//Amplify.configure(awsconfig);
+Amplify.configure({
+    Auth: {
+        identityPoolId: 'us-east-1:2ea6f0d8-fe33-43d4-8f45-445572915f1c', //REQUIRED - Amazon Cognito Identity Pool ID
+        region: 'us-east-1', // REQUIRED - Amazon Cognito Region
+       // userPoolId: 'XX-XXXX-X_abcd1234', //OPTIONAL - Amazon Cognito User Pool ID
+        //userPoolWebClientId: 'XX-XXXX-X_abcd1234', //OPTIONAL - Amazon Cognito Web Client ID
+    },
+    Storage: {
+        AWSS3: {
+            bucket: 'videobucketsprabha', //REQUIRED -  Amazon S3 bucket name
+            region: 'us-east-1', //OPTIONAL -  Amazon service region
+        }
+    }
+});
 function App() {
+      Storage.put('test.txt', 'Hello')
+    .then (result => console.log(result)) // {key: "test.txt"}
+    .catch(err => console.log(err));
   return (
     <div className="App">
       <header className="App-header">
