9957a7c72be8ccbc46e0eb2e7fbcddf8550f77fd
EC2 Default User <ec2-user@ip-172-31-80-100.ec2.internal>
2020-09-17 16:02:00 +0000

    version25

--- src/App.js
+++ src/App.js
@@ -22,18 +22,50 @@ Amplify.configure({
         }
     }
 });
-function Store_S3(filename,content){
+class App extends Component {
+  Store_S3 = async () => {
+    
+        return new Promise((resolve, reject) => {
+          var filename = 'test2.txt';
+          var file_content = 'conent for test2';
+    resolve(Storage.put(filename, file_content, {
+        level: 'private',
+        contentType: 'text/plain'
+    }));
+  });
+    
+  }
+  /*function Store_S3(filename,content){
     return new Promise((resolve, reject) => {
     resolve(Storage.put(filename, content, {
         level: 'private',
         contentType: 'text/plain'
     }));
   });
+  
 }
+*/
+ // listQuery = async () => {
+  //  const msg = await Store_S3('test1.txt','content cfor the file test1.txt');
+  //}
+    render() {
+    return (
+      <div className="App">
+        
+        <p> Click a button </p>
+        <button onClick={this.Store_S3}>Upload to S3</button>
+         
+      </div>
+    );
+  }
+}
+
+/*
 async function App() {
   try {
-    const msg = await Store_S3('test1.txt','content cfor the file test1.txt');
+    //const msg = await Store_S3('test1.txt','content cfor the file test1.txt');
       return (
+         
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -76,6 +108,7 @@ async function App() {
   );
   }
 }
+*/
  
 
 export default App;
