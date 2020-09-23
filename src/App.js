8e86f9732a2e54efd1ecbd92e50abf9e686e360f
EC2 Default User <ec2-user@ip-172-31-80-100.ec2.internal>
2020-09-23 14:48:13 +0000

    version90

--- src/App.js
+++ src/App.js
@@ -142,7 +142,7 @@ race.then((res) => alert(res)) // -> Promise A win!
         <input type="file" onChange={this.onChange} />
         <button onClick={this.Store_S3}>Upload to  S3</button>
         {this.state.vid_visible ? (
-        <video id="vid" src={window.URL.createObjectURL(this.state.file)}   width="640" height="352" controls/>
+        <video id="vid" src={ this.state.file}   width="640" height="352" controls/>
         ) : null}
          
       </div>
